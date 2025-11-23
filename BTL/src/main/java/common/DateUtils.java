90a19bb54598e10cd3379375d3d73f4da2e7f6a5
Dat Nguyen <dat3008a08@gmail.com>
2025-11-23 21:23:36 +0700

    feat(common): init basic file and date utilities

--- BTL/src/main/java/common/DateUtils.java
+++ BTL/src/main/java/common/DateUtils.java
@@ -1,4 +1,11 @@
 package common;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
+    public static String dateToString(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(date);
+    }
 }
--- BTL/src/main/java/common/FileUtils.java
+++ BTL/src/main/java/common/FileUtils.java
@@ -1,4 +1,36 @@
 package common;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileUtils {
+    public static List<String> readFile(String path) {
+        List<String> list = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader(path);
+            Scanner sc = new Scanner(fr);
+            while(sc.hasNextLine()) {
+                list.add(sc.nextLine());
+            }
+            fr.close();
+        } catch (Exception e) {
+            System.out.println("Lỗi đọc file");
+        }
+        return list;
+    }
+
+    public static void writeFile(String path, List<String> data) {
+        try {
+            FileWriter fw = new FileWriter(path);
+            for (String s : data) {
+                fw.write(s + "\n");
+            }
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
