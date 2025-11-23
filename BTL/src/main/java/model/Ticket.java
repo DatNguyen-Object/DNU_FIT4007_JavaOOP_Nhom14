575caf5dec991d9f44046dd71386c7ac99bbb210
ngovanh1908 <vietanh190806@email.com>
2025-11-23 19:03:11 +0700

    init Ticket model

--- BTL/src/main/java/model/Ticket.java
+++ BTL/src/main/java/model/Ticket.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Ticket {
+    public String id;
+    public double price;
+
+    public Ticket(String id, double price) {
+        this.id = id;
+        this.price = price;
+    }
 }
